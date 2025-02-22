c719a0764634848a1090518322e4deedd5fd17c9
noamsch5 <noamschwartz55@gmail.com>
2025-02-22 11:34:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import os
 import logging
-from src.api.youtube import find_similar_tracks
+from src.api.youtube import find_similar_tracks, analyze_keyword_realtime, get_youtube_client
 from src.api.youtube_seo import generate_seo_tags
 from src.api.keyword_analyzer import analyze_keywords
 from src.Audio.analyzer import analyze_audio
